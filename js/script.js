20378c6178d2220b6cf1b4eab78587c3bd6a6dbe
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-02 16:00:12 +0100

    problema con querySelectorAll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const eleGrill = document.getElementById('grill');
 
+
 for(i = 1; i <= 100; i++) {
     eleGrill.innerHTML += `<div class= "cell">${i}</div>`
+    
+    const eleCell = document.querySelectorAll('.cell');
+    eleCell.addEventListener('click', function() {
+    this.classList.toggle('active')
+})
 }
+
