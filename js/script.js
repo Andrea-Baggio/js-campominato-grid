3c3052cff2c74c6b9b29e53513f23b28b75d7c8c
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-02 16:26:16 +0100

    risolto problema con querySelectorAll

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@ const eleGrill = document.getElementById('grill');
 for(i = 1; i <= 100; i++) {
     eleGrill.innerHTML += `<div class= "cell">${i}</div>`
     
-    const eleCell = document.querySelectorAll('.cell');
-    eleCell.addEventListener('click', function() {
-    this.classList.toggle('active')
-})
+    
+    let eleCell = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < eleCell.length; i++) {
+        eleCell[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
 }
 
